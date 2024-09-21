773654777f9e607c5ec17506518969a2db965244
tgriffabenitez <tgriffabenitez@gmail.com>
2024-09-21 16:03:38 -0300

    init

---
+++ src/main/java/com/tmg/sensorcollector/SensorCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.tmg.sensorcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorCollectorApplication.class, args);
+    }
+
+}
